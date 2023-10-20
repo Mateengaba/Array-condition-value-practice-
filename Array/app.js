79e8679494c8109325ef9c789dabeef4de9b26d6
Mateen Gaba <mateen.gaba@gmail,com>
2023-10-20 12:31:26 +0500

    Array , if condition, value

---
+++ Array/app.js
@@ -0,0 +1,38 @@
+
+// 0        1        2   indexes
+// var fruits = ["mango", "apple", "orange"]
+// console.log(fruits[0])
+// console.log(fruits[1])
+// console.log(fruits[2])
+// console.log(fruits[4])
+
+
+// var numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// console.log(numArr)
+
+// var mixArr = ["apple", 10]
+// console.log(mixArr)
+// --------------------------
+
+
+
+// var showValue = "number"
+
+// if (showValue === "string") {
+//     console.log(mixArr[0])
+// } else {
+//     console.log(mixArr[1])
+
+// }
+
+// var stuName = ["Mateen " , "Gaba " , "Sufiyan " , "Mansoor " , "Yousuf "]
+// document.write(stuName[4])
+
+// stuName.push("Zayyan ", "Qamar")
+
+// document.write(stuName)
+
+// stuName.pop("")                  last index   (pop => remove
+// document.write(stuName)
+
+// ------------------------
---
+++ JS (if Statment)/app.js
@@ -0,0 +1,160 @@
+// var userName = "Mateen Gaba"
+
+// var userName = prompt("Enter FIRST NAME")
+// console.log("userName==>", userName)
+
+
+
+// var value1 = +prompt("Enter value 1", 50)
+// var value2 = +prompt("Enter value 2")
+
+// console.log(typeof value1)
+
+// var add = value1 + value2
+
+// console.log(add)
+
+// 1
+
+// var userAge = 20;
+
+// if (UserAge = 20){
+// console.log("Allow")}
+
+// else{ console.log ("Not Allow")}
+
+// 2
+
+// var gender = "Male";
+
+// if (gender == "Male"){
+// console.log("Allow")}
+
+// else{ console.log ("Not Allow")}
+
+
+// 3
+
+// var userAge =20;
+
+
+// if(userAge === 20){
+//     console.log("AlLow")
+// }
+// else{console.log("Not Allow") }
+
+
+// var gender = prompt("Enter your gender")
+
+// if (gender == "male") {
+//     console.log("ALLOW")
+// } else {
+//     console.log("NOT ALLOW")
+// }
+
+
+// 4
+
+// greater then
+
+
+// var userAge = +prompt("Enter your age")
+
+// if (userAge > 20){
+//     console.log("Allow")
+// }else {
+//     console.log("Not Allow")
+// }
+
+
+// less then
+
+// var userAge = +prompt("Enter your age")
+
+
+// if (userAge < 18) {
+//     console.log("USER ALLOW")
+// } else {
+//     console.log("USER NOT ALLOW")
+// }
+
+
+
+// 5
+
+// greater and equal
+
+// var userAge = + prompt("Enter your Age")
+
+// if (userAge >= 18) {
+//     console.log("USER ALLOW")
+// } else {
+//     console.log("USER NOT ALLOW")
+// }
+
+
+// less then and equal
+
+// var userAge = + prompt("Enter your Age")
+
+// if (userAge <= 18) {
+//     console.log("USER ALLOW")
+// } else {
+//     console.log("USER NOT ALLOW")
+// }
+
+
+
+// 6
+
+// User age graten then & equil to 18 && userAger <=40.
+
+// var userAge = + prompt("Enter your Age")
+
+// if (userAge >= 18 && userAge <= 40) {
+//         console.log("USER ALLOW")
+//     } else {
+//         console.log("USER NOT ALLOW")
+//     }
+
+
+// 7
+
+// User age graten then & equil to 18 and gender male or female.
+
+
+
+// var userAge = +prompt("Enter your age");
+// var userGender = prompt("Enter your gender");
+
+
+// if (userAge == 20 && userGender == "female") {
+//     console.log("USER ALLOW")
+// } else {
+//     console.log("USER NOT ALLOW")
+// }
+
+
+// var userAge = +prompt("Enter your age");
+// var userGender = prompt("Enter your gender");
+
+
+// if (userAge >= 18 && userAge <= 40 && (userGender == "male" || userGender == "female")) {
+//         console.log("USER ALLOW")
+//     } else {
+//         console.log("USER NOT ALLOW")
+//     }
+
+
+var edu = +prompt("Enter your age");
+
+
+var edu = "intermediate"
+
+if (edu == "matric") {
+    console.log("ALLOW MODULE A")
+} else if (edu === "intermediate") {
+    console.log("ALLOW MODULE B")
+} else {
+    console.log("NOT ALLOW")
+}
